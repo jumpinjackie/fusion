163094393768dab9c17b2a5c79b528f895074cd6
pspencer <pspencer@example.org>
2006-07-17 21:14:19 +0000

    update redirect.php script to keep in sync with web studio
    git-svn-id: http://svn.osgeo.org/fusion/trunk@16 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- mapguide/php/redirect.php
+++ mapguide/php/redirect.php
@@ -1,18 +1,15 @@
 <?php
 /*****************************************************************************
- *
- * $Id$
- *
- * Purpose: simple wrapper for redirecting XmlHttpRequests to a remote server
- *          to avoid javascript security limitations
- *
- * Project: MapGuide Open Source GMap demo application
- *
- * Author: DM Solutions Group Inc
+ * @project         MapGuide Open Source Chameleon
+ * @revision        $Id$
+ * @fileoverview    this file redirects requests from the client to another 
+ *                  server
+ * @author          Paul Spencer (pspencer@dmsolutions.ca)
+ * @copyright       &copy; 2006 DM Solutions Group Inc.
  *
  *****************************************************************************
  *
- * Copyright (c) 2005, DM Solutions Group Inc.
+ * Copyright (c) 2006, DM Solutions Group Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -31,58 +28,153 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
  * DEALINGS IN THE SOFTWARE.
+
+ */
+
+$user = 'Administrator';
+$pass = 'admin';
+/* very simple wrapper for redirecting requests */
+$server = parse_url($_REQUEST['s']);
+$scheme = $server['scheme'];
+$host = $server['host'];
+$method = 'POST';//$_SERVER['REQUEST_METHOD']; //'POST';
+$port = isset($server['port'])?$server['port']:80;
+$path = $server['path'];
+$url = '';
+if ($scheme == 'https') {
+    $url .= 'ssl://';
+}
+$url .= $host;
+$data = array();
+$query = '';
+$sep = '';
+foreach ($_REQUEST as $key => $value) {
+    if ($key != 's') {
+          $query .= $sep . $key .'=' .urlencode($value);
+          $sep = '&';
+          $data[$key] = rawurldecode($value);
+    }
+}
+//TODO: not necessarily xml!    
+header('Content-type: text/xml');
+$aResults = parseContent(sendToHost($url, $port, $method, $path, $data));
+
+foreach($aResults['header'] as $header) {
+  header($header);
+}
+echo implode("\n", $aResults['body']);
+    
+/*
+ * PS: 
+ * taken from
+ * http://www.faqts.com/knowledge_base/view.phtml/aid/12039/fid/51
+ * no apparent license or copyright.
+ * modified to add $port
+ * 
+ * sendToHost
+ * ~~~~~~~~~~
+ * Params:
+ *   $host      - Just the hostname.  No http:// or 
+                  /path/to/file.html portions
+ *   $method    - get or post, case-insensitive
+ *   $path      - The /path/to/file.html part
+ *   $data      - The query string, without initial question mark
+ *   $useragent - If true, 'MSIE' will be sent as 
+                  the User-Agent (optional)
  *
- *****************************************************************************/    
+ * Examples:
+ *   sendToHost('www.google.com','get','/search','q=php_imlib');
+ *   sendToHost('www.example.com','post','/some_script.cgi',
+ *              'param=First+Param&second=Second+param');
+ */
 
-//POST is broken...
-$method = 'GET';
-include('./http_post.php'); //TODO include auth credentials from a config file
+function sendToHost($host, $port, $method, $path, $data, $useragent=0) {
+    // Supply a default method of GET if the one passed was empty
+    if (empty($method)) {
+        $method = 'GET';
+    }
+    $method = strtoupper($method);
+    $fp = fsockopen($host, $port);
+    if (!$fp) {
+        echo 'fsockopen failed<BR>';
+        return;
+    }
+    if ($method == 'GET') {
+        $path .= '?' . $data;
+    }
+    
+    $header = '';
+    $body = '';
+    srand((double)microtime()*1000000);
+    $boundary = "---------------------".substr(md5(rand(0,32000)),0,10);
+    
+    $header .= "$method $path HTTP/1.0\r\n";
+    $header .= "Host: $host\r\n";
+    $header .= "Content-type: multipart/form-data, boundary=$boundary\r\n";
+    
+    // attach post vars
+    foreach($data as $index => $value){
+      // if ($index == 'content') {
+      //     $fplog = fopen('C:\\preview.log', 'w');
+      //     fwrite($fplog, $value);
+      //     fclose($fp);
+      // }
+      $body .= "--$boundary\r\n";
+      if (substr($value,0,5) == '<?xml') {
+          $body .= "Content-Disposition: form-data; name=\"".$index."\"; filename=\"blah.xml\"\r\n";
+          $body .= "Content-type: text/xml\r\n";
+      } else if ($index == 'filename') {
+          $body .= "Content-Disposition: form-data; name=\"".$index."\"; filename=\"blah.xml\"\r\n";
+          $body .= "Content-type: application/octet-stream\r\n";
+          
+          $h = fopen( $value, 'rb');
+          while (!feof($h)) {
+              $body.= fread($h, 4096);
+          }
+          //$body .= fread($h, filesize($value));
+          fclose($h);
+          //write other form vars needed for file upload to mapagent
+          $body .= "--$boundary\r\n";
+          $body .= "Content-Disposition: form-data; name=\"DATANAME\"; value=\"".$value."\"\r\n";
+          $body .= "--$boundary\r\n";
+          $body .= "Content-Disposition: form-data; name=\"DATALENGTH\"; value=\"".filesize($value)."\"\r\n";
+          continue;
+      } else {
+          $body .= "Content-Disposition: form-data; name=\"".$index."\"\r\n";
+      }
+      //if (get_magic_quotes_gpc()) {
+          $value = stripslashes($value);
+      //}
+      $body .= "\r\n".rawurldecode($value)."\r\n";
+    }
+    $body .= "--$boundary--\r\n";
+    $header .= "Content-length: " . strlen($body) . "\r\n";
+    $header .= "Connection: close\r\n\r\n";
+    
+    fputs($fp, $header.$body);
+    $buf = '';
+    while (!feof($fp)) {
+        $buf .= fgets($fp,4096);
+    }
+    fclose($fp);
+    
+    return $buf;
+}   
 
-$szResult = ($_REQUEST['s']); 
-if (strpos($szResult, '?')) { 
-	$szResult .= '&'; 
-} else { 
-	$szResult .= '?'; 
+function parseContent( $r ) {
+    $header = array();
+    $body = array();
+    $ar = explode("\n", $r);
+    foreach($ar as $idx => $line) {
+        if (trim($line) == '') {
+            $header = array_slice( $ar, 0, $idx);
+            $body = array_slice( $ar, $idx+1);
+            break;
+        }
+    }
+//     echo $r;
+//     print_r($header);
+//     print_r($body);
+    return array( 'header' => $header, 'body' => $body );
 }
-
-$sep = ''; 
-foreach ($_REQUEST as $key => $value) { 
-	if ($key != 's') { 
-		$szResult .= $sep . $key .'=' .urlencode($value);
-		$sep = '&';
-	}
-}
-
-
-//echo "<PRE>";
-//print_r ($server);
-//array_shift($_REQUEST);
-//print_r ($_REQUEST);
-//echo "</PRE>";
-//echo $szResult;
-//exit;
-//file_get_contents("http://localhost:8008/mapguide/mapagent/mapagent.fcgi?VERSION=1.0.0&OPERATION=GETRESOURCECONTENT&RESOURCEID=Library%3A%2F%2FSamples%2FSheboygan%2FLayouts%2FSheboyganPhp.WebLayout");
-//	header('Content-type: text/xml');
-//echo file_get_contents("http://localhost:8008/mapguide/mapagent/mapagent.fcgi?VERSION=1.0.0&OPERATION=GETRESOURCECONTENT&RESOURCEID=Library://Samples/Sheboygan/Layouts/SheboyganPhp.WebLayout");
-
-
-
-
-if (stristr($szResult, 'MGgenerate') !== false) {
-	header('Context-type: image/png');
-} else {
-	header('Content-type: text/xml');	
-}
-if ($method == 'GET') {           
-    echo file_get_contents($szResult);
-    exit;
-} else {
-   	$server = parse_url($_REQUEST['s']);
-    echo http_post($server['host'],
-               $server['user'],
-               $server['pass'],
-               isset($server['port'])?$server['port']:80,
-               $server['path'],
-               $_REQUEST);
-}                
 ?>
