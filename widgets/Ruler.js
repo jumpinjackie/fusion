906633da7922472679a640e035e36d393288c319
pspencer <pspencer@example.org>
2006-09-29 19:27:35 +0000

    final version of the ruler.
    git-svn-id: http://svn.osgeo.org/fusion/trunk@128 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/Ruler.js
+++ widgets/Ruler.js
@@ -4,49 +4,206 @@
  * @purpose Ruler widget
  * @author pspencer@dmsolutions.ca
  * @copyright (c) 2006 DM Solutions Group Inc.
- * @license MIT
+ * @license All Rights Reserved
  * ********************************************************************
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- * * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- * * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- ********************************************************************
+ * ********************************************************************
  *
- * Ruler widget
  * 
  * **********************************************************************/
-
 require('widgets/GxButtonBase.js');
+require('widgets/GxCanvasTool.js');
+//require('widgets/excanvas.js');
+
+var RULER_DISTANCE_CHANGED;
 
 var Ruler = Class.create();
 Ruler.prototype = 
 {
+    isDigitizing: false,
+    //distance of each segment
+    aDistances: [],
+    //cumulativeDistance distance
+    cumulativeDistance: 0,
+    lastDistance: 0,
+    
+    INCHES: 0,
+    FEET: 1,
+    MILES: 2,
+    METERS: 3,
+    KILOMETERS: 4,
+    DEGREES: 5,
+    PIXELS: 6,
+    
+    unitPerMeter: [39.37,3.2808,0.00062137,1.0,0.001,0.000009044],
+    meterPerUnit: [0.0254,0.3048,1609.3445,1.0,1000.0,110570],
+    unitNames: ['Inches', 'Feet', 'Miles', 'Meters', 'Kilometers', 'Degrees'],
+    unitAbbr: ['in', 'ft', 'mi', 'm', 'km', '&deg;'],
+    
+    /* the units to display distances in */
+    units: null,
+    
     initialize : function(oCommand)
     {
-        //console.log('Ruler.initialize');
-        Object.inheritFrom(this, GxWidget.prototype, ['Ruler', false]);
+        console.log('Ruler.initialize');
+        Object.inheritFrom(this, GxWidget.prototype, ['Ruler', true]);
         this.setMap(oCommand.getMap());
-        
         Object.inheritFrom(this, GxButtonBase.prototype, [oCommand]);
+        Object.inheritFrom(this, GxCanvasTool.prototype, [oCommand]);
+        Object.inheritFrom(this, EventMgr.prototype, []);
+        
+        this.asCursor = ['crosshair'];
+        
+        var u = oCommand.oxmlNode.getNodeText('Units');
+        switch(u.toLowerCase()) {
+            case 'inches':
+                this.units = this.INCHES;
+                break;
+            case 'feet':
+                this.units = this.FEET;
+                break;
+            case 'miles':
+                this.units = this.MILES;
+                break;
+            case 'meters':
+                this.units = this.METERS;
+            case 'kilometers':
+                this.units = this.KILOMETERS;
+                break;
+            case 'degrees':
+                this.units = this.DEGREES;
+                break;
+            default:
+                this.units = this.METERS;
+        }
+        
+        this.registerEventID(RULER_DISTANCE_CHANGED);
+    },
+    
+    /**
+     * (public) activate()
+     *
+     * activate the ruler tool
+     */
+    activateTool: function() {
+        this.getMap().activateWidget(this);
+        this._oButton.activateTool();
+    },
+    
+    
+    activate: function() {
+        this.activateCanvas();
+    },
+    
+    /**
+     * (public) deactivate()
+     *
+     * deactivate the ruler tool
+     */
+    deactivate: function() {
+        //console.log('Ruler.deactivate');
+        this.isDigitizing = false;
+        this.clearContext();
+        this._oButton.deactivateTool();
+        this.deactivateCanvas();
+    },
+    
+    /**
+     * (public) mouseDown(e)
+     *
+     * handle the mouse down event
+     *
+     * @param e Event the event that happened on the mapObj
+     */
+    mouseDown: function(e) {
+        //console.log('Ruler.mouseDown');
+        var p = this.getMap().getEventPosition(e);
 
+        if (!this.isDigitizing) {
+            this.currentFeature = new FeatureLine();
+            this.lastDistance = 0;
+            this.cumulativeDistance = 0;
+            this.aDistances = [];
+            var from = new Node(p.x,p.y);
+            var to = new Node(p.x,p.y);
+            var seg = new Segment(from,to);
+            seg.setEditing(true);
+            this.currentFeature.addSegment(seg);
+            this.clearContext();
+            this.currentFeature.draw(this.context);     
+            this.isDigitizing = true;
+        } else {
+            var seg = this.currentFeature.lastSegment();
+            var d = this.measureSegment(seg);
+            this.aDistances.push(d);
+            this.cumulativeDistance += d;
+            this.lastDistance = 0;
+            this.triggerEvent(RULER_DISTANCE_CHANGED, this, this.getDistance());
+            seg.setEditing(false);
+            seg = this.currentFeature.extendLine();
+            seg.setEditing(true);
+            this.clearContext();
+            this.currentFeature.draw(this.context);
+        }
     },
 
     /**
-     * called when the button is clicked by the GxButtonBase widget
+     * (public) mouseMove(e)
+     *
+     * handle the mouse move event
+     *
+     * @param e Event the event that happened on the mapObj
      */
-    activateTool : function()
-    {
-        //console.log('FitToWindow.activateTool');
+    mouseMove: function(e) {
+        //console.log('Digitizer.mouseMove');
+        if (!this.isDigitizing) return;
+    
+        var p = this.getMap().getEventPosition(e);
+        var seg = this.currentFeature.lastSegment();
+        seg.to.set(p.x,p.y);
+        this.clearContext();
+        this.currentFeature.draw(this.context);
+        this.updateDistance(seg);
+    },
+
+    /**
+     * (public) dblClick(e)
+     *
+     * handle the mouse dblclick event
+     *
+     * @param e Event the event that happened on the mapObj
+     */
+    dblClick: function(e) {
+        //console.log('Digitizer.dblClick');
+        if (!this.isDigitizing) return;
+    
+        var p = this.getMap().getEventPosition(e);
+        var seg = this.currentFeature.lastSegment();
+        seg.setEditing(false);
+        seg.to.set(p.x,p.y);
+        this.clearContext();
+        this.currentFeature.draw(this.context);
+        this.isDigitizing = false;
+    },
+    
+    measureSegment: function(seg) {
+        var map = this.getMap();
+        var s = map.pixToGeo(seg.from.x, seg.from.y);
+        var e = map.pixToGeo(seg.to.x, seg.to.y);
+        var geoDist = Math.sqrt(Math.pow(e.x-s.x,2) + Math.pow(e.y-s.y,2));
+        var meters = geoDist * map._fMetersperunit;
+        return meters;
+    },
+    
+    updateDistance: function(seg) {
+        this.lastDistance = this.measureSegment(seg);
+        this.triggerEvent(RULER_DISTANCE_CHANGED, this, this.getDistance());
+    },
+    
+    getDistance: function() {
+        var totalDistance = this.cumulativeDistance + this.lastDistance;
+        if (this.units != this.METERS) {
+            totalDistance = totalDistance * this.unitPerMeter[this.units];
+        }
+        return totalDistance;
     }
 };
