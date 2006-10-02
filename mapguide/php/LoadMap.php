9608824753ba1c26f43f86d305ccb56ab49dde50
pspencer <pspencer@example.org>
2006-10-02 02:02:01 +0000

    minor changes to use MGCommon instead
    git-svn-id: http://svn.osgeo.org/fusion/trunk@147 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- mapguide/php/LoadMap.php
+++ mapguide/php/LoadMap.php
@@ -32,106 +32,79 @@
  * DEALINGS IN THE SOFTWARE.
  *
  *****************************************************************************/
-$extensionDir = getcwd() . "/../../../";
-$viewDir = $extensionDir."mapviewerphp/";
+include('MGCommon.php');
 
 try
 {
+    $mappingService = $siteConnection->CreateService(MgServiceType::MappingService);
 
-include $viewDir . "common.php";
-include $viewDir . "constants.php";
-;
-
-MgInitializeWebTier($extensionDir. "webconfig.ini");
-//MgInitializeWebTier("c:/Program Files/MapGuideOpenSource/WebServerExtensions/www/webconfig.ini");
-
-
- $sessionId = $_REQUEST['session'];
-//$user = new MgUserInformation('Administrator', 'admin');
-$user = new MgUserInformation($_REQUEST['session']);
+    // Get a runtime map from a map definition
+    if (isset($_REQUEST['mapid']))
+    {
+      $mapid = $_REQUEST['mapid'];
+      //echo $mapid;
+      $resourceID = new  MgResourceIdentifier($mapid);
+    }
 
 
+    //make a copy of the map in the session so we can make temporary changes to it
+    $contentReader = $resourceService->GetResourceContent($resourceID);
 
-$siteConnection = new MgSiteConnection();
-$siteConnection->Open($user);
+    //$resourceService->SetResource($resourceID, $contentReader, null);
 
+    $map = new MgMap();
+    $mapName = $resourceID->GetName();
 
+    //echo "<br> maname $mapName <br>";
 
-
-
-$resourceService = $siteConnection->CreateService(MgServiceType::ResourceService);
-$mappingService = $siteConnection->CreateService(MgServiceType::MappingService);
-
-// Get a runtime map from a map definition
-if (isset($_REQUEST['mapid']))
-{
-  $mapid = $_REQUEST['mapid'];
-  //echo $mapid;
-  $resourceID = new  MgResourceIdentifier($mapid);
-}
+    $map->Create($resourceService, $resourceID, $mapName);
 
 
-//make a copy of the map in the session so we can make temporary changes to it
-$contentReader = $resourceService->GetResourceContent($resourceID);
-
-//$resourceService->SetResource($resourceID, $contentReader, null);
+    $mapStateId = new MgResourceIdentifier("Session:" . $sessionID . "//" . $map->GetName() . "." . MgResourceType::Map);
 
 
+    //create an empty selection object and store it in the session repository
+    $sel = new MgSelection($map);
+    $sel->Save($resourceService, $mapName);
 
 
-$map = new MgMap();
-$mapName = $resourceID->GetName();
-
-//echo "<br> maname $mapName <br>";
-
-$map->Create($resourceService, $resourceID, $mapName);
+    $map->Save($resourceService, $mapStateId);
 
 
-$mapStateId = new MgResourceIdentifier("Session:" . $sessionId . "//" . $map->GetName() . "." . MgResourceType::Map);
+    //$sessionId =  $map->GetSessionId();
+    $mapName = $map->GetName() ;
+    $extents = $map->GetMapExtent();
+    @$oMin = $extents->GetLowerLeftCoordinate();
+    @$oMax = $extents->GetUpperRightCoordinate();
+
+    @$srs = $map->GetMapSRS();
+    if($srs != "")
+    {
+      @$csFactory = new MgCoordinateSystemFactory();
+      @$cs = $csFactory->Create($srs);
+      @$metersPerUnit = $cs->ConvertCoordinateSystemUnitsToMeters(1.0);
+      //  $unitsType = $cs->GetUnits();
+    }
+    else
+    {
+      $metersPerUnit = 1.0;
+      //$unitsType = "Meters";
+    }   
 
 
-//create an empty selection object and store it in the session repository
-$sel = new MgSelection($map);
-$sel->Save($resourceService, $mapName);
-
-
-$map->Save($resourceService, $mapStateId);
-
-
-//$sessionId =  $map->GetSessionId();
-$mapName = $map->GetName() ;
-$extents = $map->GetMapExtent();
-@$oMin = $extents->GetLowerLeftCoordinate();
-@$oMax = $extents->GetUpperRightCoordinate();
-
-@$srs = $map->GetMapSRS();
-if($srs != "")
-{
-  @$csFactory = new MgCoordinateSystemFactory();
-  @$cs = $csFactory->Create($srs);
-  @$metersPerUnit = $cs->ConvertCoordinateSystemUnitsToMeters(1.0);
-  //  $unitsType = $cs->GetUnits();
-}
-else
-{
-  $metersPerUnit = 1.0;
-  //$unitsType = "Meters";
-}   
-
-
-header('content-type: text/xml');
-echo "<mapguidesession>";
-echo "<sessionid>$sessionId</sessionid>";
-echo "<mapid>$mapid</mapid>";
-echo "<metersperunit>$metersPerUnit</metersperunit>";
-echo "<mapname>$mapName</mapname>";
-echo "<extent>";
-echo "<minx>".$oMin->GetX()."</minx>";
-echo "<miny>".$oMin->GetY()."</miny>";
-echo "<maxx>".$oMax->GetX()."</maxx>";
-echo "<maxy>".$oMax->GetY()."</maxy>";
-echo "</extent>";
-echo "</mapguidesession>";
+    header('content-type: text/xml');
+    echo "<mapguidesession>";
+    echo "<sessionid>$sessionId</sessionid>";
+    echo "<mapid>$mapid</mapid>";
+    echo "<metersperunit>$metersPerUnit</metersperunit>";
+    echo "<mapname>$mapName</mapname>";
+    echo "<extent>";
+    echo "<minx>".$oMin->GetX()."</minx>";
+    echo "<miny>".$oMin->GetY()."</miny>";
+    echo "<maxx>".$oMax->GetX()."</maxx>";
+    echo "<maxy>".$oMax->GetY()."</maxy>";
+    echo "</extent>";
+    echo "</mapguidesession>";
 }
 catch (MgException $e)
 {
