00ccb7f19469f630f90fb907d7739205876d0230
pspencer <pspencer@example.org>
2006-11-15 22:04:29 +0000

    new file
    git-svn-id: http://svn.osgeo.org/fusion/trunk@311 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

---
+++ widgets/SelectPolygon.js
@@ -0,0 +1,195 @@
+/********************************************************************** * 
+ * @project Fusion
+ * @revision $Id$
+ * @purpose Select by polygon widget
+ * @author pspencer@dmsolutions.ca
+ * @copyright (c) 2006 DM Solutions Group Inc.
+ * @license MIT
+ * ********************************************************************
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ********************************************************************
+ *
+ * perform a selection using a polygon
+ * 
+ * **********************************************************************/
+
+Fusion.require('widgets/GxButtonBase.js');
+Fusion.require('widgets/GxCanvasTool.js');
+
+var SelectPolygon = Class.create();
+SelectPolygon.prototype = 
+{       
+    nTolerance : 3, //default pixel tolernace for a point click
+    initialize : function(oCommand)
+    {
+        //console.log('Select.initialize');
+        Object.inheritFrom(this, GxWidget.prototype, ['SelectPolygon', true]);
+        Object.inheritFrom(this, GxButtonBase.prototype, [oCommand]);
+        Object.inheritFrom(this, GxCanvasTool.prototype, [this.getMap()]);
+        this.setMap(oCommand.getMap());
+        this.asCursor = ['auto'];
+
+        if (parseInt(oCommand.oxmlNode.getNodeText('Tolerance')) > 0)
+        {
+            nTolerance = parseInt(oCommand.oxmlNode.getNodeText('Tolerance'));
+        }
+        this.polygon = new FeaturePolygon();
+    },
+    
+    /**
+     * called when the button is clicked by the MGButtonBase widget
+     */
+    activateTool : function()
+    {
+        this.getMap().activateWidget(this);
+        //this.activate();
+    },
+
+    /**
+     * activate the widget (listen to mouse events and change cursor)
+     * This function should be defined for all functions that register
+     * as a widget in the map
+     */
+    activate : function()
+    {
+        this.activateCanvas();
+        this.getMap().setCursor(this.asCursor);
+        /*icon button*/
+        this._oButton.activateTool();
+        this.polygon = new FeaturePolygon(this.getMap());
+    },
+
+    /**
+     * deactivate the widget (listen to mouse events and change cursor)
+     * This function should be defined for all functions that register
+     * as a widget in the map
+     **/
+    deactivate : function()
+    {
+         this.deactivateCanvas();
+         this.getMap().setCursor('auto');
+         /*icon button*/
+         this._oButton.deactivateTool();
+    },
+    
+    /**
+     * (public) mouseDown(e)
+     *
+     * handle the mouse down event
+     *
+     * @param e Event the event that happened on the mapObj
+     */
+    mouseDown: function(e) {
+        //console.log('SelectRadius.mouseDown');
+        if (Event.isLeftClick(e)) {
+            var p = this.getMap().getEventPosition(e);
+
+            if (!this.isDigitizing) {
+                var point = this.getMap().pixToGeo(p.x, p.y);
+                var from = new Node(point.x,point.y, this.getMap());
+                var to = new Node(point.x,point.y, this.getMap());
+                var seg = new Segment(from,to);
+                seg.setEditing(true);
+                this.polygon.addSegment(seg);
+                this.clearContext();
+                this.polygon.draw(this.context);     
+
+                this.isDigitizing = true;
+            } else {
+                var seg = this.polygon.lastSegment();
+                seg.setEditing(false);
+                seg = this.polygon.extendLine();
+                seg.setEditing(true);
+                this.clearContext();
+                this.polygon.draw(this.context);
+            }
+        }
+    },
+
+    /**
+     * (public) mouseMove(e)
+     *
+     * handle the mouse move event
+     *
+     * @param e Event the event that happened on the mapObj
+     */
+    mouseMove: function(e) {
+        //console.log('SelectRadius.mouseMove');
+        if (!this.isDigitizing) return;
+    
+        var p = this.getMap().getEventPosition(e);
+        var seg = this.polygon.lastSegment();
+        seg.to.setPx(p.x,p.y);
+        seg.to.updateGeo();
+        this.clearContext();
+        this.polygon.draw(this.context);
+    },
+    
+    /**
+     * (public) dblClick(e)
+     *
+     * handle the mouse dblclick event
+     *
+     * @param e Event the event that happened on the mapObj
+     */
+    dblClick: function(e) {
+        //console.log('Digitizer.dblClick');
+        if (!this.isDigitizing) return;
+        this.event = e;
+        var p = this.getMap().getEventPosition(e);
+        var point = this.getMap().pixToGeo(p.x, p.y);
+        var seg = this.polygon.lastSegment();
+        seg.setEditing(false);
+        seg.to.set(point.x,point.y);
+        this.clearContext();
+        this.isDigitizing = false;
+        this.execute();
+    },
+
+    /**
+     *  
+     **/
+    execute : function() {
+        var wkt = 'POLYGON((';
+        var nodes = this.polygon.getNodes();
+        var sep = '';
+        for (var i=0; i<nodes.length; i++) {
+            wkt = wkt + sep + nodes[i].x + ' ' + nodes[i].y;
+            sep = ',';
+        }
+        wkt = wkt + sep + nodes[0].x + ' ' + nodes[0].y + '))';
+        
+        var options = {};
+        options.geometry = wkt;
+        options.selectionType = "inside";
+
+        if (this.bActiveOnly) {
+            var layer = this.getMap().getActiveLayer();
+            if (layer) {
+                options.layers = layer.layerName;
+            } else {
+                return;
+            }
+        }
+        
+        if (this.event.shiftKey) {
+            options.extendSelection = true;
+        }
+        
+        this.getMap().query(options);
+    }
+};
---
+++ widgets/SelectRadiusValue.js
@@ -0,0 +1,92 @@
+/********************************************************************** * 
+ * @project Fusion
+ * @revision $Id$
+ * @purpose Manually enter a radius for the SelectRadius
+ * @author pspencer@dmsolutions.ca
+ * @copyright (c) 2006 DM Solutions Group Inc.
+ * @license MIT
+ * ********************************************************************
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ ********************************************************************
+ *
+ * The user can manually enter a radius for the SelectRadius widget.
+ *
+ * **********************************************************************/
+
+var SelectRadiusValue = Class.create();
+SelectRadiusValue.prototype = 
+{
+    initialize : function(oCommand)
+    {
+        Object.inheritFrom(this, GxWidget.prototype, ['SelectRadiusValue', false]);
+        this.setMap(oCommand.getMap());
+        
+        this.radiusWidgetName = oCommand.oxmlNode.getNodeText('RadiusName');
+        this.label = oCommand.oxmlNode.getNodeText('Label');
+        this.className = oCommand.oxmlNode.getNodeText('ClassName');
+        
+        this.input = document.createElement('input');
+        this.input.type = 'text';
+        
+        this.domObj = document.createElement('div');
+        this.domObj.className = this.className;
+        if (this.label != '') {
+            var span = document.createElement('span');
+            span.innerHTML = this.label;
+            this.domObj.appendChild(span);
+        }
+        this.domObj.appendChild(this.input);
+        
+        if (oCommand.getName() != '') {
+            $(oCommand.getName()).appendChild(this.domObj);
+        }
+        Event.observe(this.input, 'blur', this.onBlur.bind(this));
+        this.getMap().registerForEvent(MAP_LOADED, this.mapLoaded.bind(this));
+        this.getMap().registerForEvent(MAP_EXTENTS_CHANGED, this.mapExtentsChanged.bind(this));
+    },
+    
+    mapLoaded: function() {
+        var widgets = Fusion.getWidgetsByType('SelectRadius');
+        for (var i=0; i<widgets.length; i++) {
+            if (widgets[i].sName == this.radiusWidgetName) {
+                this.widget = widgets[i];
+            }
+        }
+        this.updateFromWidgetValue();
+    },
+    
+    mapExtentsChanged: function() {
+        this.updateWidgetValue();
+    },
+    
+    onBlur: function() {
+        this.updateWidgetValue();
+    },
+    
+    updateWidgetValue: function() {
+        if (this.widget) {
+            var radius = this.getMap().geoToPixMeasure(this.input.getValue());
+            this.widget.setRadius(radius);
+        }
+    },
+    
+    updateFromWidgetValue: function() {
+        if (this.widget) {
+            this.input.value = this.getMap().pixToGeoMeasure(this.widget.defaultRadius);
+        }
+    }
+};
