b47f7cd4368f6fae2ce36a639c8a7d16e913f568
assefa <assefa@example.org>
2007-03-25 23:10:40 +0000

    mapserver session.
    git-svn-id: http://svn.osgeo.org/fusion/trunk@520 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- mapserver/php/CreateSession.php
+++ mapserver/php/CreateSession.php
@@ -1,12 +1,13 @@
 <?php
 /* initialize a session and return the session id to the caller */
 include(dirname(__FILE__).'/Common.php');
+
 initializeSession( "sid", "", "" );
 $sessionId = session_id();
 
 header('content-type: text/xml');
 echo "<!--".getSessionSavePath()."-->";
-echo "<mapguidesession>";
+echo "<mapserveresession>";
 echo "<sessionid>$sessionId</sessionid>";
-echo "</mapguidesession>";
+echo "</mapserveresession>";
 ?>
