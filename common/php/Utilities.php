7a6b5dbc3672cf9d9b4236487b5c49e0a22f9ba5
pspencer <pspencer@example.org>
2007-04-03 18:49:22 +0000

    removed extraneous code
    git-svn-id: http://svn.osgeo.org/fusion/trunk@551 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- common/php/Utilities.php
+++ common/php/Utilities.php
@@ -101,4 +101,86 @@ function xml2json($domElement) {
     }
     return $result;
 }
+
+/**
+ * this function determines if a path represents an absolute path and returns
+ * true if it is, and false if it is not
+ * @param szPath string the path to test
+ * @result boolean true if the path is absolute, false otherwise
+ */
+function isAbsolutePath( $szPath ) {
+    if ($szPath == "") { return false; }
+    if ($szPath[0] == "/" || preg_match('/^(\w:)/', $szPath)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+/**
+ * This function translate $szDestPath (relative or absolute)
+ * to a absolute path based on $szOrigPath.
+ *
+ * @param $szDestPath string Destination path to translate
+ * @param $szOrigPath string Reference path
+ */
+function resolvePath2 ($szDestPath, $szOrigPath) {
+    // If one or other path is missing or absolute, return it.
+    if ($szDestPath == "") { return $szOrigPath; }
+    if ($szOrigPath == "") { return $szDestPath; }
+    if ($szDestPath[0] == "/" || preg_match('/^(\w:)/', $szDestPath)) {
+        //echo "absolute: $szDestPath<BR>";
+        return $szDestPath;
+    }
+
+    // If windows prefix (eg: c:) get it
+    $szPrefix = "";
+    if ($szOrigPath[0] != "/") {
+        if (preg_match('/^(\w:)(.*)/i', $szOrigPath, $aMatch)) {
+            $szPrefix = $aMatch[1];
+            $szOrigPath = $aMatch[2];
+        } else {
+            $szOrigPath = "/".$szOrigPath;
+        }
+    }
+
+    // Make sure path finishing with "/"
+    if ($szOrigPath[strlen($szOrigPath)-1] != "/" &&
+        !is_dir($szOrigPath)) {
+        $szOrigPath = dirname($szOrigPath)."/";
+    }
+    if ($szOrigPath[strlen($szOrigPath)-1] != "/") {
+        $szOrigPath = $szOrigPath."/";
+    }
+    $szPath = $szOrigPath.$szDestPath;
+
+    // Remove repetitive "/"
+    $szPath = ereg_replace ("/+", "/", $szPath);
+
+    $szPath = iterate_ereg_replace ("/\./", "/", $szPath);
+    $szPath = iterate_ereg_replace ("/[^(/|\.)]+/\.\./", "/", $szPath);
+
+    //Rest of the function
+    return $szPrefix.$szPath;
+}
+
+/**
+ * Recursive ereg_replace
+ *
+ * @param $szPattern string Regular expression to process
+ * @param $szReplacement string Value to replace all matching result with
+ * @param $szString string Haystack to process on
+ *
+ * @return string szString with the matching result replaced.
+ */
+function iterate_ereg_replace ( $szPattern, $szReplacement, $szString) {
+    $szResult = $szString;
+    do {
+        $szString = $szResult;
+        $szResult = ereg_replace ($szPattern, $szReplacement, $szString);
+    } while ($szResult != $szString);
+
+    return $szResult;
+}
 ?>
