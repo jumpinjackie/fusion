5aa3da0147365d6dde8667af8b33f731ba54ed1e
pspencer <pspencer@example.org>
2007-04-03 13:22:09 +0000

    fix escape problems with sending XML
    git-svn-id: http://svn.osgeo.org/fusion/trunk@549 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- common/php/Xml2JSON.php
+++ common/php/Xml2JSON.php
@@ -8,19 +8,16 @@ if (!isset($_POST['xml'])) {
     die('xml not set');
 }
 
-$xml = urldecode($_POST['xml']);
+$xml = $_POST['xml'];
+$xml = str_replace('\"', '"', $xml);
+
 $document = DOMDocument::loadXML($xml);
 if ($document == null) {
     die ('/* invalid xml document:'.$xml.' */');
 }
 $root = $document->documentElement;
-$result = "{";
-$result .= '"'.$root->tagName.'":';
-$result .= xml2json($root);
-$result .= '}';
 
 header('Content-type: text/plain');
 header('X-JSON: true');
-
-echo $result;
+echo '{"' . $root->tagName . '":' . xml2json($root) . '}';
 ?>
