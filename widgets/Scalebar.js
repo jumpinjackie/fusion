31bb6a5ce192379f48a49655ea805d24b396051a
pspencer <pspencer@example.org>
2007-06-26 15:43:02 +0000

    make a bunch of paramters configurable
    git-svn-id: http://svn.osgeo.org/fusion/trunk@645 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/Scalebar.js
+++ widgets/Scalebar.js
@@ -25,30 +25,74 @@
  * **********************************************************************/
 
 
-Fusion.require('widgets/scalebartool.js');
+Fusion.require('widgets/scalebar/scalebartool.js');
 
 var Scalebar = Class.create();
-Scalebar.prototype = 
-{
-    oCommad : null,
-
-    initialize : function(oCommand)
-    {
+Scalebar.prototype = {
+    style: 'thin',
+    displaySystem: 'metric',
+    minWidth: 100,
+    maxWidth: 200,
+    divisions: 2,
+    subdivisions: 2,
+    showMinorMeasures: true,
+    abbreviateLabel: true,
+    singleLine: false,
+    initialize : function(oCommand) {
         //console.log('Scalebar.initialize');
         Object.inheritFrom(this, GxWidget.prototype, ['Scalebar', false, oCommand]);
         this.setMap(oCommand.getMap());
         
         this.oCommand = oCommand;
+        
+        var json = oCommand.jsonNode;
+        this.style = json.Style ? json.Style[0].toLowerCase() : this.style;
+        if (this.style != 'fancy' && 
+            this.style != 'fat' && 
+            this.style != 'thin' && 
+            this.style != 'thinner') {
+            this.style = 'thin';
+        }
+        
+        this.displaySystem = json.DisplaySystem ? json.DisplaySystem[0] : this.displaySystem;
+        this.minWidth = json.MinWidth ? json.MinWidth[0] : this.minWidth;
+        this.maxWidth = json.MaxWidth ? json.MaxWidth[0] : this.maxWidth;
+        this.divisions = json.Divisions ? json.Divisions[0] : this.divisions;
+        this.subdivisions = json.SubDivisions ? json.SubDivisions[0] : this.subdivisions;
+        this.hideInvisibleLayers = (json.HideInvisibleLayers && json.HideInvisibleLayers[0]) == 'true' ? true : false;
+        this.showMinorMeasures = (json.ShowMinorMeasures && json.ShowMinorMeasures[0]) == 'false' ? false : true;
+        this.abbreviateLabel = (json.AbbreviateLabel && json.AbbreviateLabel[0]) == 'true' ? true : false;
+        this.singleLine = (json.SingleLine && json.SingleLine[0]) == 'true' ? true : false;
+        
+        
+        if (document.styleSheets) {
+            if (document.styleSheets[0]) {
+                var url = Fusion.getFusionURL() + 'widgets/scalebar/scalebar-'+this.style+'.css';
+                console.log(url);
+                if (document.styleSheets[0].addImport) {
+                    document.styleSheets[0].addImport(url);
+                } else {
+                    document.styleSheets[0].insertRule('@import url('+url+');',0);
+                }
+            }
+        }
 
-        this.oScaleBar = new ScaleBarTool(this.getMap().getScale());
+        this.oScaleBar = new ScaleBarTool(1);
+        this.oScaleBar.displaySystem = this.displaySystem
+        this.oScaleBar.minWidth = this.minWidth;
+        this.oScaleBar.maxWidth = this.maxWidth;
+        this.oScaleBar.divisions = this.divisions;
+        this.oScaleBar.subdivisions = this.subdivisions;
+        this.oScaleBar.showMinorMeasures = this.showMinorMeasures;
+        this.oScaleBar.abbreviateLabel = this.abbreviateLabel;
+        this.oScaleBar.singleLine = this.singleLine;
         this.oScaleBar.place(oCommand.getName());
 
-
         this.getMap().registerForEvent(MAP_EXTENTS_CHANGED, this.extentsChangedCB.bind(this));
+        this.getMap().registerForEvent(MAP_LOADED, this.extentsChangedCB.bind(this));
     },
 
-    extentsChangedCB : function()
-    {
+    extentsChangedCB : function() {
         this.oScaleBar.update(this.getMap().getScale());
     }
 };
