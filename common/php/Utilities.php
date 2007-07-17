b1a2cf9984888629dadd5d2e8a378a5f7463f1f3
zak <zak@example.org>
2007-07-17 15:45:47 +0000

    added var2json so it is accessible everywhere
    git-svn-id: http://svn.osgeo.org/fusion/trunk@692 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- common/php/Utilities.php
+++ common/php/Utilities.php
@@ -1,4 +1,36 @@
 <?php
+
+/* recursively convert a variable to its json representation */
+function var2json($var) {
+    $result = "";
+    if (is_object($var)) {
+        $result .= "{";
+        $sep = "";
+        foreach($var as $key => $val) {
+            $result .= $sep.'"'.$key.'":'.var2json($val);
+            $sep = ",";
+        }
+        $result .= "}";
+    } else if (is_array($var)) {
+        $result .= "[";
+        $sep = "";
+        for($i=0; $i<count($var); $i++) {
+            $result .= $sep.var2json($var[$i]);
+            $sep = ",";
+        }
+        $result .= "]";
+    } else if (is_string($var)) {
+        $result = "'".str_replace("\n", '\n', $var)."'";
+    } else if (is_bool($var)) {
+        $result = $var ? 'true' : 'false';
+    } else if (is_null($var)) {
+        $result = 'null';
+    }else {
+        $result = $var;
+    }
+    return $result;
+}
+
 function xml2json($domElement) {
     $result = '';
     if ($domElement->nodeType == XML_COMMENT_NODE) {
