ba50f391dde0cf62c42ac8ecb1a1860d8789322a
madair <madair@example.org>
2007-07-18 20:52:46 +0000

    remove printLayout and menu stuff
    git-svn-id: http://svn.osgeo.org/fusion/trunk@697 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/SaveMap.js
+++ widgets/SaveMap.js
@@ -50,8 +50,6 @@
 Fusion.Widget.SaveMap = Class.create();
 Fusion.Widget.SaveMap.prototype = {
     iframe : null,
-    oMenu : null,
-    isMenuWidget : false,
     printLayout : null,
     printScale : null,
     initialize : function(oCommand)
@@ -65,52 +63,12 @@ Fusion.Widget.SaveMap.prototype = {
         
         //for DWF, parse printLayouts and build menu
         if (this.format == 'DWF' && oCommand.jsonNode.PrintLayout.length) {
-            var opt = {label:this._oButton._sLabel};
-            this.isMenuWidget = true;
-            this.oMenu = new Jx.Menu(opt);
             
-            var layouts = oCommand.jsonNode.PrintLayout;
-            for (var i = 0; i < layouts.length; i++) {
-                var layout = layouts[i];
-                var opt = {};
-                opt.label = layout.Name[0];
-                var data = layout.ResourceId[0];
-                var menuItem = null;
-                if (layout.Scale) {
-                    //create entries for weblayout specified scales
-                    menuItem = new Jx.Menu(opt);
-                    for (var j=0; j < layout.Scale.length; j++) {
-                        var scale = layout.Scale[j];
-                        var scaleAction = new Jx.Action(this.setLayout.bind(this, data, scale));
-                        var subMenuItem = new Jx.MenuItem(scaleAction,{label:scale});
-                        menuItem.add(subMenuItem);
-                    }
-                    //add an entry for current scale
-                    var currentScaleAction = new Jx.Action(this.setLayout.bind(this, data));
-                    var currentScaleItem = new Jx.MenuItem(currentScaleAction,
-                                                         {label:'Current Scale'});
-                    menuItem.add(currentScaleItem);
-                } else {
-                    //if there are no scales, the layout is used with current scale
-                    var action = new Jx.Action(this.setLayout.bind(this, data));
-                    menuItem = new Jx.MenuItem(action,opt);
-                }
-                this.oMenu.add(menuItem);
-            }
+            this.printLayout = oCommand.jsonNode.PrintLayout[0];
+            this.printScale =  this.printLayout.Scale[0];
         }
 
         this.enable = Fusion.Widget.SaveMap.prototype.enable;
-    },
-    
-    getMenu: function() {
-        return this.oMenu;
-    },
-    setLayout: function(rid) {
-        if (arguments.length > 1) {
-            this.printScale = parseInt(arguments[1]);
-        }
-        this.printLayout = rid;
-        this.activateTool();
     },
     
     enable: function() {
