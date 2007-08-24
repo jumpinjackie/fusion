39c8d02520e1940587aab31f71c4c5cfbea880f5
pspencer <pspencer@example.org>
2007-08-24 19:47:25 +0000

    re #14, #15: fixed up task pane help and css, and pass menu item action to widget so it can potentially disable the menu item if necessary.
    git-svn-id: http://svn.osgeo.org/fusion/trunk@726 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/TaskPane.js
+++ widgets/TaskPane.js
@@ -1,4 +1,4 @@
-/********************************************************************** *
+ /****************************************************************************
  * @project Fusion
  * @revision $Id: Legend.js 710 2007-07-30 13:38:36Z pspencer $
  * @purpose TaskPane widget
@@ -18,57 +18,9 @@
  * 
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
- ********************************************************************
+ *****************************************************************************
  *
- * TaskPane and layer control
- *
- * To put a TaskPane control in your application, you first need to add a
- * widget to your WebLayout as follows:
- *
- * <Command xsi:type="LegendCommandType">
- *   <Name>MyLegend</Name>
- *   <Label>Legend</Label>
- *   <TargetViewer>All</TargetViewer>
- *   <Action>Legend</Action>
- *   <ShowRootFolder>false</ShowRootFolder>
- *   <LayerThemeIcon>images/tree_map.png</LayerThemeIcon>
- *   <DisabledLayerIcon>images/tree_layer.png</DisabledLayerIcon>
- *   <RootFolderIcon>images/tree_map.png</RootFolderIcon>
- * </Command>
- *
- * The important parts of this Command are:
- *
- * Name (string, mandatory)
- *
- * an element with an id that is the same as this name must be in
- * the application.  For instance:
- *
- * <div id="MyLegend"></div>
- *
- * The legend will appear inside the element you provide.
- *
- * ShowRootFolder (boolean, optional)
- *
- * This controls whether the tree will have a single root node that
- * contains the name of the map as its label.  By default, the root
- * node does not appear.  Set to "true" or "1" to make the root node
- * appear.
- *
- * RootFolderIcon: (string, optional)
- *
- * The url to an image to use for the root folder.  This only has an
- * affect if ShowRootFolder is set to show the root folder.
- *
- * LayerThemeIcon: (string, optional)
- *
- * The url to an image to use for layers that are currently themed.
- *
- * DisabledLayerIcon: (string, optional)
- *
- * The url to an image to use for layers that are out of scale.
- *
- * **********************************************************************/
-//Fusion.require('jx/tree/jxtree.js');
+ ****************************************************************************/
 
 Fusion.Widget.TaskPane = Class.create();
 Fusion.Widget.TaskPane.prototype =
@@ -90,39 +42,50 @@ Fusion.Widget.TaskPane.prototype =
         this.defPrevTaskIcon = 'images/icon_back.gif';
         this.defNextTaskIcon = 'images/icon_forward.gif';
         this.defTaskListIcon = 'images/icon_tasks.gif';
-       
+        this.defInitialTask = oCommand.sLocation + '/TaskPane/TaskPane.html';
        
         this._oDomObj = $(oCommand.getName());
        
         var json = oCommand.jsonNode;
        
+        var taskURL = json.InitialTask ? json.InitialTask[0] : this.defInitialTask;
+        
+        if (taskURL.indexOf("http://")<0) {
+            if (taskURL.slice(0,1) == "/") {
+                taskURL = window.location.protocol + "//" + window.location.host + taskURL;
+            } else {
+                taskURL = Fusion.getFusionURL() + taskURL;
+            }
+        }
+        this.initialTask = taskURL;
+        
         var divName = 'TaskNav';
         var tmpDiv = document.createElement('div');
         tmpDiv.setAttribute('id', divName);
         this.toolbar = new Jx.Toolbar(tmpDiv,{left:0});
 
-        this.homeAction = new Jx.Action(this.gotoFirstTask.bind(this));
+        this.homeAction = new Jx.Action(this.goHome.bind(this));
         this.toolbar.add(new Jx.Button(this.homeAction, 
-                    {
-                    image: this.defHomeIcon, 
-                    tooltip: 'return to the task pane home'
-                    }
+            {
+            image: this.defHomeIcon, 
+            tooltip: 'return to the task pane home'
+            }
         ));
 
         this.prevAction = new Jx.Action(this.gotoPrevTask.bind(this));
         this.toolbar.add(new Jx.Button(this.prevAction, 
-                    {
-                    image: this.defPrevTaskIcon, 
-                    tooltip: 'go to previous task executed'
-                    }
+            {
+            image: this.defPrevTaskIcon, 
+            tooltip: 'go to previous task executed'
+            }
         ));
 
         this.nextAction = new Jx.Action(this.gotoNextTask.bind(this));
         this.toolbar.add(new Jx.Button(this.nextAction, 
-                    {
-                    image: this.defNextTaskIcon, 
-                    tooltip: 'go to next task executed'
-                    }
+            {
+            image: this.defNextTaskIcon, 
+            tooltip: 'go to next task executed'
+            }
         ));
 
         this.taskMenu = new Jx.Menu({image: this.defTaskListIcon, label: 'Task List', right:0});
@@ -148,40 +111,43 @@ Fusion.Widget.TaskPane.prototype =
         //we need to trigger an initial resize after the panel
         //is added to the DOM
         this.oTaskPane.domObj.resize();
+        this.setContent(this.initialTask);
         
         Fusion.registerForEvent(Fusion.Event.FUSION_INITIALIZED, this.setTaskMenu.bind(this));
     },
     
     updateButtons: function() {
-        this.homeAction.setEnabled(this.nTasks > 0);
         this.prevAction.setEnabled(this.nCurrentTask > 0);
         this.nextAction.setEnabled(this.nCurrentTask < this.aExecutedTasks.length - 1);
     },
     
     gotoPrevTask: function() {
-      this.nCurrentTask = this.nCurrentTask>0 ? --this.nCurrentTask : 0;
-      this.iframe.src = this.aExecutedTasks[this.nCurrentTask];
-      this.updateButtons();
+        this.nCurrentTask = this.nCurrentTask>0 ? --this.nCurrentTask : 0;
+        this.iframe.src = this.aExecutedTasks[this.nCurrentTask];
+        this.updateButtons();
     },
 
     gotoNextTask: function() {
-      this.nCurrentTask = this.nCurrentTask<this.aExecutedTasks.length-1 ? 
+        this.nCurrentTask = this.nCurrentTask<this.aExecutedTasks.length-1 ? 
                           ++this.nCurrentTask : this.aExecutedTasks.length-1;
-      this.iframe.src = this.aExecutedTasks[this.nCurrentTask];
-      this.updateButtons();
+        this.iframe.src = this.aExecutedTasks[this.nCurrentTask];
+        this.updateButtons();
     },
 
-    gotoFirstTask: function() {
-      this.nCurrentTask = 0;
-      this.iframe.src = this.aExecutedTasks[this.nCurrentTask];
-      this.updateButtons();
+    goHome: function() {
+        this.nCurrentTask = 0;
+        this.iframe.src = this.aExecutedTasks[this.nCurrentTask];
+        this.updateButtons();
     },
 
     setContent: function(url) {
-      this.aExecutedTasks.push(url);
-      ++this.nCurrentTask;
-      this.iframe.src = url;
-      this.updateButtons();
+        if (this.nCurrentTask < this.aExecutedTasks.length) {
+            this.aExecutedTasks.splice(this.nCurrentTask, this.aExecutedTasks.length - this.nCurrentTask)
+        }
+        this.aExecutedTasks.push(url);
+        ++this.nCurrentTask;
+        this.iframe.src = url;
+        this.updateButtons();
     },
 
     /**
@@ -190,18 +156,16 @@ Fusion.Widget.TaskPane.prototype =
      *
      */
     setTaskMenu : function() {
-      var taskWidgets = Fusion.getWidgetsByType("InvokeURL");
-      for (var i=0; i<taskWidgets.length; ++i) {
-        var task = taskWidgets[i];
-        if (task.sTarget == this.sName) {
-          var taskAction = new Jx.Action(task.execute.bind(task));
-          this.taskMenu.add( new Jx.MenuItem(taskAction, {label: task.sLabel, image: task.sImageURL}));
-          this.nTasks ++;
-          if (this.nCurrentTask < 0) {
-              task.execute();
-          }
+        var taskWidgets = Fusion.getWidgetsByType("InvokeURL");
+        for (var i=0; i<taskWidgets.length; ++i) {
+            var task = taskWidgets[i];
+            if (task.sTarget == this.sName) {
+                var taskAction = new Jx.Action(task.execute.bind(task));
+                this.taskMenu.add( new Jx.MenuItem(taskAction, {label: task.sLabel, image: task.sImageURL}));
+                task.action = taskAction;
+                this.nTasks ++;
+            }
         }
-      }
     }
    
 };
