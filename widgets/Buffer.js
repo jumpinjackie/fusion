620724e94ff663fa2a55dfee6e41f7d891f086d6
zak <zak@example.org>
2007-12-19 18:03:28 +0000

    update for OpenLayers-based map widget compatibility
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1134 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/Buffer.js
+++ widgets/Buffer.js
@@ -207,15 +207,21 @@ Fusion.Widget.Buffer.prototype = {
             fillColor += this.fillColor;
         }
         
-        var s = this.getMap().arch + '/' + Fusion.getScriptLanguage() + "/Buffer." + Fusion.getScriptLanguage() ;
+        var mapWidget = this.getMap();
+        var aMaps = mapWidget.getAllMaps();        
+        var s = aMaps[0].arch + '/' + Fusion.getScriptLanguage() + "/Buffer." + Fusion.getScriptLanguage();
         var params = {};
-        params.parameters = 'session='+this.getMap().getSessionID()+'&mapname='+ this.getMap().getMapName()+layer+distance+borderColor+fillColor; 
+        params.parameters = 'locale='+Fusion.locale +
+                            '&session='+aMaps[0].getSessionID() +
+                            '&mapname='+ aMaps[0].getMapName()+
+                            layer+distance+borderColor+fillColor; 
         params.onComplete = this.bufferCreated.bind(this);
         Fusion.ajaxRequest(s, params);
     },
     
     bufferCreated: function() {
-        this.getMap().reloadMap();
-        this.getMap().drawMap();
+        var aMaps = this.getMap().getAllMaps();
+        aMaps[0].reloadMap();
+        aMaps[0].drawMap();
     }
 };
