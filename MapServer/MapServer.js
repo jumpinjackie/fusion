4dd8373da0a072c94e2cfc701b2b10088ba8701c
madair <madair@example.org>
2008-01-08 16:11:48 +0000

    re #18: implementation of I18n for javascript files
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1142 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- MapServer/MapServer.js
+++ MapServer/MapServer.js
@@ -333,7 +333,7 @@ Fusion.Maps.MapServer.prototype = {
             this.mapWidget.triggerEvent(Fusion.Event.MAP_RELOADED);
         } else {
             Fusion.reportError( new Fusion.Error(Fusion.Error.FATAL, 
-						'Failed to load requested map:\n'+r.responseText));
+                OpenLayers.String.translate('mapLoadError', r.responseText)));
         }
         this.mapWidget._removeWorker();
     },
@@ -371,7 +371,7 @@ Fusion.Maps.MapServer.prototype = {
   				this.drawMap();
   				this.triggerEvent(Fusion.Event.MAP_LAYER_ORDER_CHANGED);
   			} else {
-  				alert("setLayers failure:"+o.layerindex);
+          alert(OpenLayers.String.translate('setLayersError', o.layerindex));
   			}
       }
     },
