2363cad0ebb1c4763c10dbf7da104539cd30dfd0
madair <madair@example.org>
2008-04-09 17:50:36 +0000

    exclude redline layers from queries; exception handlers should return HTTP status 200
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1369 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- MapGuide/php/Common.php
+++ MapGuide/php/Common.php
@@ -118,7 +118,6 @@ try {
     echo "</Exception>";
     exit;
 } catch (MgUserNotFoundException $unfe) {
-    header("HTTP/1.0 500 Internal Server Error");
     header('Content-type: text/xml');
     echo "<Exception>";
     echo "<Type>User Not Found</Type>";
