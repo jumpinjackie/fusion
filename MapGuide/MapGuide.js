3583859d63d921e753089508f0777de002e7329c
madair <madair@example.org>
2008-09-29 18:11:27 +0000

    closes [https://trac.osgeo.org/fusion/ticket/113]: if maxScale is Infinity, set it to an actual value;  set a maxExtent for panning in AppDef MapWIdget extension. patch applied to trunk
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1565 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- MapGuide/MapGuide.js
+++ MapGuide/MapGuide.js
@@ -535,11 +535,10 @@ Fusion.Maps.MapGuide = OpenLayers.Class(Fusion.Lib.EventMgr, {
       var layerOptions = {
         units: this.units,
         isBaseLayer: bIsBaseLayer,
-        maxExtent: this._oMaxExtent,
+        maxExtent: this.mapWidget.maxExtent ? this.mapWidget.maxExtent : this._oMaxExtent,
         maxResolution: 'auto',
         useOverlay: this.selectionAsOverlay,
         ratio: this.ratio
-//        transitionEffect : 'resize'
       };
       if (!/WebKit/.test(navigator.userAgent)) {
         layerOptions.transitionEffect = 'resize';
@@ -551,6 +550,8 @@ Fusion.Maps.MapGuide = OpenLayers.Class(Fusion.Lib.EventMgr, {
       }
       if (this.maxScale != Infinity) {
         layerOptions.minScale = this.maxScale;    //OL interpretation of min/max scale is reversed from Fusion
+      } else {
+        layerOptions.minScale = Fusion.REALLY_SMALL_SCALE;
       }
       //only set both max and min scale when not using scales array
       if (!this.mapWidget.oMapOL.scales && !this.scales) {
