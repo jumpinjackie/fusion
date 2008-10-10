12724b3f0c09936f4327c0187fee79aacbaf1175
pagameba <pagameba@example.org>
2008-10-10 13:54:40 +0000

    updating to latest jx build
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1588 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- jx/jxlib.uncompressed.js
+++ jx/jxlib.uncompressed.js
@@ -6151,12 +6151,13 @@ Jx.Chrome = new Class({
             c.setStyle('backgroundImage', 'none');
 
             /* make chrome */
-            ['TL','TR','BL','BR'].each(function(s){
+            ['TR','TL','BL','BR'].each(function(s){
                 c.adopt(
                     new Element('div',{
                         'class':'jxChrome'+s
                     }).adopt(
                     new Element('img',{
+                        'class':'png24',
                         src:src
                     }))
                 );
@@ -6809,6 +6810,12 @@ Jx.Button.Flyout = new Class({
             vertical: ['bottom top', 'top bottom'],
             offsets: this.chromeOffsets
         });
+        
+        /* we have to size the container for IE to render the chrome correctly
+         * there is some horrible peekaboo bug in IE 6
+         */
+        this.contentContainer.setContentBoxSize($(this.content).getMarginBoxSize());
+        
         this.contentContainer.setStyle('visibility','');
 
         window.addEvent('keypress', this.keypressWatcher);
@@ -9990,8 +9997,8 @@ Jx.Panel = new Class({
         if (this.options.id) {
             this.domObj.id = this.options.id;
         }
-        if ($(this.options.parent)) {
-            $(this.options.parent).adopt(this.domObj);
+        if (this.options.parent) {
+            this.addTo(this.options.parent);
         }
         var jxl = new Jx.Layout(this.domObj, $merge(this.options, {propagate:false}));
         var layoutHandler = this.layoutContent.bind(this);
@@ -10008,22 +10015,15 @@ Jx.Panel = new Class({
         
         for (var i=0; i<this.toolbars.length; i++) {
             var tb = this.toolbars[i];
-            tb.addEvent('add', layoutHandler);
-            tb.addEvent('remove', layoutHandler);
             var position = tb.options.position;
             var tbc = this.toolbarContainers[position];
             if (!tbc) {
-                var tbc = new Element('div', {
-                    'class':'jxBarContainer '+'jxBar'+position.capitalize()
-                });
+                var tbc = new Element('div');
                 new Jx.Layout(tbc);
-                var clearer = new Element('div', {'class':'jxClearer'});
-                tbc.appendChild(clearer);
                 this.contentContainer.adopt(tbc);
                 this.toolbarContainers[position] = tbc;
             }
-            var tbObj = tb.domObj;
-            tbc.insertBefore(tbObj, tbc.lastChild);
+            tb.addTo(tbc);
         }
         
         this.content = new Element('div', {
@@ -10042,10 +10042,6 @@ Jx.Panel = new Class({
         this.addEvent('addTo', function() {
             this.domObj.resize();
         });
-        
-        if (this.options.parent) {
-            this.addTo(this.options.parent);
-        }
     },
     
     layoutContent: function() {
@@ -12244,7 +12240,7 @@ Jx.Button.Tab = new Class({
  * This file is licensed under an MIT style license
  */
 Jx.Toolbar = new Class({
-    Implements: [Options,Events, Jx.Addable],
+    Implements: [Options,Events],
     /**
      * Property: items
      * {Array} an array of the things in the toolbar.
@@ -12265,7 +12261,9 @@ Jx.Toolbar = new Class({
     options: {
         type: 'Toolbar',
         position: 'top',
-        parent: null
+        parent: null,
+        autoSize: false,
+        scroll: false
     },
     /**
      * Constructor: Jx.Toolbar
@@ -12283,7 +12281,7 @@ Jx.Toolbar = new Class({
      */
     initialize : function(options) {
         this.setOptions(options);
-        this.addEvent('addTo', this.onAddTo.bind(this));
+        this.items = [];
         
         this.domObj = new Element('ul', {
             id: this.options.id,
@@ -12298,34 +12296,20 @@ Jx.Toolbar = new Class({
             this.add(this.options.items);
         }
     },
-    /**
-     *
-     */
-    onAddTo: function() {
-        var owner = $(this.domObj.parentNode);
-        if (!owner) {
-            return;
+    
+    addTo: function(parent) {
+        var tbc = $(parent).retrieve('jxBarContainer');
+        if (!tbc) {
+            tbc = new Jx.Toolbar.Container({
+                parent: parent, 
+                position: this.options.position, 
+                autoSize: this.options.autoSize,
+                scroll: this.options.scroll
+            });
         }
-        if (!owner.hasClass('jxBarContainer')) {
-            owner.addClass('jxBarContainer');
-        }
-        if (['top','right','bottom','left'].contains(this.options.position)) {
-            owner.addClass('jxBar' +
-                           this.options.position.capitalize());            
-        } else {
-            owner.addClass('jxBarTop');
-        }            
-        if (!owner.getChildren().some(function(child){
-            if (child.hasClass('jxClearer')) {
-                child.inject(owner);
-                return true;
-            } else {
-                return false;
-            }
-        })) {
-            owner.adopt(new Element('div', {'class':'jxClearer'}));                                        
-        }
+        tbc.add(this);
     },
+    
     /**
      * Method: add
      * Add an item to the toolbar.  If the item being added is a Jx component
@@ -12339,7 +12323,6 @@ Jx.Toolbar = new Class({
      */
     add: function( ) {
         $A(arguments).flatten().each(function(thing) {
-            thing.toolbar = this;
             if (thing.domObj) {
                 thing = thing.domObj;
             }
@@ -12506,6 +12489,218 @@ Jx.Toolbar.Separator = new Class({
         this.domObj.appendChild(this.domSpan);
     }
 });
+// $Id: $
+/**
+ * Class: Jx.Toolbar.Container
+ * A toolbar container contains toolbars.  A single toolbar container fills the
+ * available space horizontally.  Toolbars placed in a toolbar container do not
+ * wrap when they exceed the available space.
+ *
+ * Events:
+ * add - fired when one or more toolbars are added to a container
+ * remove - fired when one or more toolbars are removed from a container
+ *
+ * Implements: 
+ * Options
+ * Events
+ * {<Jx.Addable>}
+ *
+ * License: 
+ * Copyright (c) 2008, DM Solutions Group Inc.
+ * 
+ * This file is licensed under an MIT style license
+ */
+Jx.Toolbar.Container = new Class({
+    Implements: [Options,Events, Jx.Addable],
+    /**
+     * Property: domObj
+     * {HTMLElement} the HTML element that the container lives in
+     */
+    domObj : null,
+    options: {
+        parent: null,
+        position: 'top',
+        autoSize: false,
+        scroll: true
+    },
+    /**
+     * Constructor: Jx.Toolbar.Container
+     * Create a new instance of Jx.Toolbar.Container
+     *
+     * Parameters:
+     * options - an options object as documented below
+     *
+     * Options:
+     * id - {String} the ID to give the main DIV element created by the 
+     *     container
+     * parent - {HTMLElement} object reference or id to place the toolbar in.
+     */
+    initialize : function(options) {
+        this.setOptions(options);
+        
+        var d = $(this.options.parent);
+        this.domObj = d || new Element('div');
+        this.domObj.addClass('jxBarContainer');
+        
+        if (this.options.scroll) {
+            this.scroller = new Element('div', {'class':'jxBarScroller'});
+            this.domObj.adopt(this.scroller);
+        }
+
+        /* this allows toolbars to add themselves to this bar container
+         * once it already exists without requiring an explicit reference
+         * to the toolbar container
+         */
+        this.domObj.store('jxBarContainer', this);
+        
+        if (['top','right','bottom','left'].contains(this.options.position)) {
+            this.domObj.addClass('jxBar' +
+                           this.options.position.capitalize());            
+        } else {
+            this.domObj.addClass('jxBarTop');
+        }
+        
+        this.clearer = new Element('div', {'class':'jxClearer'});
+        
+        if (this.options.scroll) {
+            // make sure we update our size when we get added to the DOM
+            this.addEvent('addTo', this.update.bind(this));
+            
+            this.scroller.adopt(this.clearer);
+        
+            var scrollFx = new Fx.Tween(this.scroller);
+            this.scrollLeft = new Jx.Button({
+                image: Jx.aPixel.src
+            }).addTo(this.domObj);
+
+            this.scrollLeft.domObj.addClass('jxBarScrollLeft');
+            this.scrollLeft.addEvents({
+               click: (function(){
+                   var from = this.scroller.getStyle('left').toInt();
+                   var to = Math.min(from+100, 0);
+                   if (to >= 0) {
+                       this.scrollLeft.domObj.setStyle('display', 'none');
+                   }
+                   this.scrollRight.domObj.setStyle('display', 'block');
+                   scrollFx.start('left', from, to);
+               }).bind(this)
+            });
+    
+            this.scrollRight = new Jx.Button({
+                image: Jx.aPixel.src
+            }).addTo(this.domObj);
+            this.scrollRight.domObj.addClass('jxBarScrollRight');
+            this.scrollRight.addEvents({
+               click: (function(){
+                   var from = this.scroller.getStyle('left').toInt();
+                   var to = Math.max(from - 100, this.scrollWidth);
+                   if (to == this.scrollWidth) {
+                       this.scrollRight.domObj.setStyle('display', 'none');
+                   }
+                   this.scrollLeft.domObj.setStyle('display', 'block');
+                   scrollFx.start('left', from, to);
+               }).bind(this)
+            });            
+        } else {
+            this.domObj.adopt(this.clearer);
+        }
+
+        
+        if (this.options.toolbars) {
+            this.add(this.options.toolbars);
+        } 
+    },
+    
+    update: function() {
+        if (this.options.autoSize) {
+            /* delay the size update a very small amount so it happens
+             * after the current thread of execution finishes.  If the
+             * current thread is part of a window load event handler,
+             * rendering hasn't quite finished yet and the sizes are
+             * all wrong
+             */
+            (function(){
+                var x = 0;
+                this.scroller.getChildren().each(function(child){
+                    x+= child.getSize().x;
+                });
+                this.domObj.setStyles({width:x});
+                this.measure();
+            }).delay(1,this);
+        } else {
+            this.measure();
+        }
+    },
+    
+    measure: function() {
+        /* decide if we need to show the scroller buttons and
+         * do some calculations that will make it faster
+         */
+        this.scrollWidth = this.domObj.getSize().x;
+        this.scroller.getChildren().each(function(child){
+            this.scrollWidth -= child.getSize().x;
+        }, this);
+        if (this.scrollWidth < 0) {
+            var l = this.scroller.getStyle('left').toInt();
+            if (l < 0) {
+                this.scrollLeft.domObj.setStyle('display','block');
+            } else {
+                this.scrollLeft.domObj.setStyle('display','none');
+            }
+            if (l == this.scrollWidth) {
+                this.scrollRight.domObj.setStyle('display', 'none');
+            } else {
+                this.scrollRight.domObj.setStyle('display', 'block');                
+            }
+        } else {
+            this.scrollLeft.domObj.setStyle('display','none');
+            this.scrollRight.domObj.setStyle('display','none');
+        }            
+    },
+    
+    /**
+     * Method: add
+     * Add a toolbar to the container.
+     *
+     * Parameters:
+     * toolbar - {Object} the toolbar to add.  More than one toolbar
+     *    can be added by passing multiple arguments.
+     */
+    add: function( ) {
+        $A(arguments).flatten().each(function(thing) {
+            if (this.options.scroll) {
+                /* we potentially need to show or hide scroller buttons
+                 * when the toolbar contents change
+                 */
+                thing.addEvent('add', this.update.bind(this));
+                thing.addEvent('remove', this.update.bind(this));                
+            }
+            thing.domObj.inject(this.clearer, 'before');
+        }, this);
+        if (this.options.scroll) {
+            this.update();            
+        }
+        if (arguments.length > 0) {
+            this.fireEvent('add', this);
+        }
+        return this;
+    },
+    /**
+     * Method: remove
+     * remove an item from a toolbar.  If the item is not in this toolbar
+     * nothing happens
+     *
+     * Parameters:
+     * item - {Object} the object to remove
+     *
+     * Returns:
+     * {Object} the item that was removed, or null if the item was not
+     * removed.
+     */
+    remove: function(item) {
+        
+    }
+});
 // $Id: treeitem.js 803 2008-09-02 18:02:13Z pspencer $
 /**
  * Class: Jx.TreeItem 
@@ -12628,6 +12823,7 @@ Jx.TreeItem = new Class ({
         }
         //this.domA.removeEvents();
         this.options = null;
+        this.domObj.dispose();
         this.domObj = null;
         this.owner = null;
     },
@@ -12789,6 +12985,7 @@ Jx.TreeFolder = new Class({
     finalize: function() {
         this.finalizeFolder();
         this.finalizeItem();
+        this.subDomObj.dispose();
         this.subDomObj = null;
     },
     /**
@@ -12799,7 +12996,6 @@ Jx.TreeFolder = new Class({
         this.domObj.childNodes[0].removeEvents();
         for (var i=this.nodes.length-1; i>=0; i--) {
             this.nodes[i].finalize();
-            if (this.nodes[i].domObj) this.subDomObj.removeChild(this.nodes[i].domObj);
             this.nodes.pop();
         }
         
