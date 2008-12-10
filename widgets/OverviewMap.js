f98a3da9fb6a810ba3e3f55a3d4bc5494a0ebacd
pagameba <pagameba@example.org>
2008-12-10 20:34:08 +0000

    re #175, update to the latest version of jxlib.
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1707 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/OverviewMap.js
+++ widgets/OverviewMap.js
@@ -71,10 +71,7 @@ Fusion.Widget.OverviewMap = OpenLayers.Class(Fusion.Widget, {
             jxl.addEvent('sizeChange', OpenLayers.Function.bind(this.sizeChanged, this));
         }
         
-        Fusion.addWidgetStyleSheet(widgetTag.location + 'OverviewMap/OverviewMap.css');
-        this.oMapOptions = {
-            theme: null
-        };  //TODO: allow setting some mapOptions in AppDef
+        this.oMapOptions = {};  //TODO: allow setting some mapOptions in AppDef
 
         //this.getMap().registerForEvent(Fusion.Event.MAP_LOADED, OpenLayers.Function.bind(this.mapWidgetLoaded, this));
     },
