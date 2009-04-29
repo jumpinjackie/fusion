a40e2de7e8ec4cc1a23bd02ed98885a8caba4c32
pagameba <pagameba@example.org>
2009-04-29 18:05:43 +0000

    Re [https://trac.osgeo.org/fusion/ticket/250], add version-specfic templates for theming as the schema has changed for AreaRule in some versions of the layer schema.  Also clean up some of the PHP so it doesn't output blank text.
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1840 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/Theme/thememain.php
+++ widgets/Theme/thememain.php
@@ -16,10 +16,6 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
-
-?>
-
-<?php
     $fusionMGpath = '../../layers/MapGuide/php/';
     require_once $fusionMGpath . 'Common.php';
     require_once $fusionMGpath . 'Utilities.php';
