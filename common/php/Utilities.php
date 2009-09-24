7b3241df642b843e4c29069ab5f384da6dfe2c3f
pagameba <pagameba@example.org>
2009-09-24 19:09:10 +0000

    Closes [https://trac.osgeo.org/fusion/ticket/81].  Modify var2json to always utf8 encode json strings.
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1911 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- common/php/Utilities.php
+++ common/php/Utilities.php
@@ -26,35 +26,40 @@
 
 /* recursively convert a variable to its json representation */
 function var2json($var) {
-    $result = "";
-    if (is_object($var)) {
-        $result .= "{";
-        $sep = "";
-        foreach($var as $key => $val) {
-            $result .= $sep.'"'.$key.'":'.var2json($val);
-            $sep = ",";
-        }
-        $result .= "}";
-    } else if (is_array($var)) {
-        $result .= "[";
-        $sep = "";
-        for($i=0; $i<count($var); $i++) {
-            $result .= $sep.var2json($var[$i]);
-            $sep = ",";
-        }
-        $result .= "]";
-    } else if (is_string($var)) {
-        //$tmpStr = str_replace("'", "\'", $var);
-        $tmpStr = str_replace('"', '\"', $var);
-        $result = '"'.str_replace("\n", '\n', $tmpStr).'"';
-    } else if (is_bool($var)) {
-        $result = $var ? 'true' : 'false';
-    } else if (is_null($var)) {
-        $result = 'null';
-    }else {
-        $result = $var;
+    if (function_exists('json_encode')) {
+        $result json_encode($var);
+    } else {
+        $result = "";
+        if (is_object($var)) {
+            $result .= "{";
+            $sep = "";
+            foreach($var as $key => $val) {
+                $result .= $sep.'"'.$key.'":'.var2json($val);
+                $sep = ",";
+            }
+            $result .= "}";
+        } else if (is_array($var)) {
+            $result .= "[";
+            $sep = "";
+            for($i=0; $i<count($var); $i++) {
+                $result .= $sep.var2json($var[$i]);
+                $sep = ",";
+            }
+            $result .= "]";
+        } else if (is_string($var)) {
+            //$tmpStr = str_replace("'", "\'", $var);
+            $tmpStr = str_replace('"', '\"', $var);
+            $result = '"'.str_replace("\n", '\n', $tmpStr).'"';
+        } else if (is_bool($var)) {
+            $result = $var ? 'true' : 'false';
+        } else if (is_null($var)) {
+            $result = 'null';
+        } else {
+            $result = $var;
+        }      
     }
-    return $result;
+
+    return utf8_encode($result);
 }
 
 function xml2json($domElement) {
