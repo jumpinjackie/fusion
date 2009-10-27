5117f435451fe0a1f2d7f387326075cb9238c201
chrisclaydon <chrisclaydon@example.org>
2009-10-27 21:02:28 +0000

    Fix #296 - FeatureInfo, Query and Theme widgets should be marked as "exclusive" to allow them to play nicely with other widgets that require the task pane
    git-svn-id: http://svn.osgeo.org/fusion/trunk@1955 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/FeatureInfo.js
+++ widgets/FeatureInfo.js
@@ -36,6 +36,7 @@
 
 
 Fusion.Widget.FeatureInfo = OpenLayers.Class(Fusion.Widget, {
+    isExclusive: true,
     uiClass: Jx.Button,
     sFeatures : 'menubar=no,location=no,resizable=no,status=no',
 
