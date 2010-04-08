290282c6f8f5a95e78fdd2b3e1109855ebc56a1b
chrisclaydon <chrisclaydon@example.org>
2010-04-08 18:02:52 +0000

    Fixes [https://trac.osgeo.org/fusion/ticket/384] - Buffer / Theme cause the map to zoom to its initial extents when commercial base map layers are present in the map.
    git-svn-id: http://svn.osgeo.org/fusion/trunk@2138 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- widgets/Theme/thememain.php
+++ widgets/Theme/thememain.php
@@ -359,7 +359,7 @@
                 themeReqHandler = null;
 
                 var map = parent.Fusion.getMapByName(mapName);
-                map.reloadMap();
+                map.reloadNamedMap(mapName);
 
             }
         }
