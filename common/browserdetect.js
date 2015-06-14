30ab13c56ab3bccf172db38f63f60b7ccd4dd2c7
Jackie Ng <jumpinjackie@gmail.com>
2015-06-14 14:08:48 +0000

    Merged revision(s) 2915 from trunk: [https://trac.osgeo.org/fusion/ticket/633]: Update browserdetect.js ........
    git-svn-id: http://svn.osgeo.org/fusion/branches/fusion-mg30@2917 1f2f6c64-2f36-0410-af3b-c496fd8ebf2b

--- common/browserdetect.js
+++ common/browserdetect.js
@@ -1,12 +1,16 @@
 var agent = navigator.userAgent.toLowerCase();
 var firefox = agent.indexOf("firefox") != -1;
 var opera = agent.indexOf("opera") != -1;
+var chrome = agent.indexOf("chrome") != -1;
 
 var safariIndex = agent.indexOf("safari");
 var safari1or2 = false;
 var safari3plus = false;
 var safari = false;
-if(safariIndex != -1)
+
+var macOS = navigator.appVersion.indexOf("Mac")!=-1;
+
+if(!chrome && safariIndex != -1)
 {
     safari = true;
     var safariVersion = agent.substr(safariIndex + 7, 3);
@@ -27,7 +31,8 @@ var msie6minus = false;
 if(msieIndex != -1)
 {
     msie = true;
-    var msieVersion = agent.substr(msieIndex + 5, 1);
+    var msieVersion = agent.substr(msieIndex + 5, 2);
+
     if(parseFloat(msieVersion) >= 7)
     {
         msie7plus = true;
@@ -36,4 +41,13 @@ if(msieIndex != -1)
     {
         msie6minus = true;
     }
+}
+
+function getAttributeNodeValue(obj,attr){
+
+    if(obj[attr])
+        return obj[attr];
+    else
+        return obj.attributes[attr].nodeValue;
+
 }
